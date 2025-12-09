6fb0a162a69dc832be68ecfdf3bd8cb563bc72ea
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-12-09 20:19:51 +0430

    Config and install packages

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider dir='rtl'>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
