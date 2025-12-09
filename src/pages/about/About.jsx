9742f16be6ead3b289569c8f2df3a3c7e8a777b7
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-12-09 20:56:51 +0430

    Project Structure: Components and Pages

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,11 @@
+import './About.css'
+
+function About() {
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  );
+}
+
+export default About
---
+++ src/pages/blog/Blog.jsx
@@ -0,0 +1,11 @@
+import './Blog.css'
+
+function Blog() {
+  return (
+    <>
+      <h1>Blog Page</h1>
+    </>
+  );
+}
+
+export default Blog
