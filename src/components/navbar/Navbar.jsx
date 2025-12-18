31aad8d45a1e1f675695ecec6f64322792f0a865
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-12-19 02:53:01 +0430

    About Page: Just created.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import './Navbar.css'
+import "./Navbar.css";
 
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -14,10 +14,14 @@ function MyNavbar() {
         key={expand}
         expand={expand}
         style={{ backgroundColor: "rgb(186, 182 ,253)" }}
-        className="mb-3"
       >
-        <Container >
-          <Navbar.Brand href="#" style={{fontFamily:'Lalezar' , fontSize: '27px'}}>افغانستان من</Navbar.Brand>
+        <Container>
+          <Navbar.Brand
+            href="#"
+            style={{ fontFamily: "Lalezar", fontSize: "27px" }}
+          >
+            افغانستان من
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
@@ -31,11 +35,21 @@ function MyNavbar() {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <NavLink to='/' className='nav-link'>صفحه اصلی</NavLink>
-                <NavLink to='/about' className='nav-link'>درباره ما</NavLink>
-                <NavLink to='/blog' className='nav-link'>مقالات</NavLink>
-                <NavLink to='/panel' className='nav-link'>پنل</NavLink>
-                <NavLink to='/login' className='nav-link'>ورود</NavLink>
+                <NavLink to="/" className="nav-link">
+                  صفحه اصلی
+                </NavLink>
+                <NavLink to="/about" className="nav-link">
+                  درباره ما
+                </NavLink>
+                <NavLink to="/blog" className="nav-link">
+                  مقالات
+                </NavLink>
+                <NavLink to="/panel" className="nav-link">
+                  پنل
+                </NavLink>
+                <NavLink to="/login" className="nav-link">
+                  ورود
+                </NavLink>
               </Nav>
 
               {/* <Nav className="justify-content-end flex-grow-1 pe-3">
@@ -45,7 +59,6 @@ function MyNavbar() {
                   <NavLink to="/panel" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>پنل</NavLink>
                   <NavLink to="/login" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>ورود</NavLink>
                </Nav>   */}
-
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
