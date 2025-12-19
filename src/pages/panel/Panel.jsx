7cd581428c2395d932793926dfaaaf920d234212
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-12-19 21:35:09 +0430

    Authentication for Panel page: If user is admin he can see panel page! else navigate to home

--- src/pages/panel/Panel.jsx
+++ src/pages/panel/Panel.jsx
@@ -1,12 +1,24 @@
+import { Navigate } from "react-router-dom";
 import MyNavbar from "../../components/navbar/Navbar";
 import "./Panel.css";
 
 function Panel() {
+  const isLogin = (username) => {
+    if (username == "admin") return true;
+    return false;
+  };
+
   return (
     <>
-      <MyNavbar />
+      {isLogin("adsmin") ? (
+        <div>
+          <MyNavbar />
 
-      <h1>Panel Page</h1>
+          <h1>Panel Page</h1>
+        </div>
+      ) : (
+        <Navigate to="/" />
+      )}
     </>
   );
 }
